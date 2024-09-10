72399ae29c1b9a15baaa85d695f441408696bf93
ericdu6865 <zhengyuandu2005311@gmail.com>
2024-09-10 18:49:24 -0400

    profile picutres, abouteme

---
+++ app.js
@@ -0,0 +1,41 @@
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1300); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+});
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetID = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetID);
+  
+      window.scrollTo({
+        top: targetSection.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
